25a101e773bb4f0d64aef8aa0bb6d40afd1664e8
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-04-12 10:06:44 -0700

    Change function with const

--- Stack.h
+++ Stack.h
@@ -5,8 +5,8 @@
 *       -Fast operations: O(1) push(), pop(), and top()                      *
 *       -Built in list functions and size tracking are helpful               *
 *    Disadvantages:                                                          *
-*       -Can only remove from the top one at a time                          *
-*       -No way to access anything in the middle or beginning                *
+*       -Can only remove from the top and one at a time from the stack       *
+*       -No way to access anything in the middle or beginning of the stack   *
 *       -Lists are known for their poor reference locality                   *
 *****************************************************************************/
 
@@ -22,7 +22,7 @@ class Stack{
         std::list<T> stack_;
     public:
         Stack();
-        Stack(int& capacity, const T& data);
+        Stack(const int& capacity, const T& data);
         Stack(const Stack<T>& other);
         ~Stack();
         T& operator=(const Stack<T>& other);
